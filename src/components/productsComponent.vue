14635a420e1abe3a4d86ae1672c1a37a9fe39c21
Florent Chazot <florent.chazot@gmail.com>
2019-01-23 10:43:09 +0100

    affichage product list

--- src/components/productsComponent.vue
+++ src/components/productsComponent.vue
@@ -1,22 +1,21 @@
 <template>
   <div class="border">
-      <div v-if="products !== null">
-        <div class="product-list mid_height" v-bind:class="{ fullHeight: isFullHeight}">
-          <div v-bind:key="product.id" v-for="product in products">
-            <div  class="product-item" v-on:click="select(product)">
-              <img :alt="product.title" class="product-image" :src="'http://image.tmdb.org/t/p/w154' + product.coverUrl">
-              <div class="product-title">{{product.title}}</div>
-            </div>
-          </div>
-          <!--<br>-->
-          <div class="list-bottom-container">
-            <button v-if="products !== null && products !== []" v-on:click="getNextResultPages(2)" class="list-bottom" ref="bottom">Afficher la suite...</button>
+    <div v-if="products.length != 0">
+      <div class="product-list">
+        <div v-bind:key="product.id" v-for="product in products">
+          <div  class="product-item" v-on:click="select(product)">
+            <img :alt="product.title" class="product-image" :src="'http://image.tmdb.org/t/p/w154' + product.coverUrl">
+            <div class="product-title">{{product.title}}</div>
           </div>
         </div>
+        <div class="list-bottom-container">
+          <button v-on:click="getNextResultPages(2)" class="list-bottom" ref="bottom">Afficher la suite...</button>
+        </div>
       </div>
-      <!--<div class="loader-container" v-else>-->
-        <!--&lt;!&ndash;<ring-loader color="#2c3e50" size="200px"></ring-loader>&ndash;&gt;-->
-      <!--</div>-->
+    </div>
+    <div v-else>
+
+    </div>
   </div>
 </template>
 
@@ -71,6 +70,7 @@ export default {
         this.products = []
       } else {
         this.products.push(newValue)
+        console.log(this.products)
       }
     },
     getSelected(newValue) {
@@ -109,50 +109,19 @@ export default {
     font-weight: bold;
     color: #2c3e50;
   }
-
   .border{
     position: relative;
-    /*flex-direction: column;*/
-    /*flex-flow: wrap;*/
-    /*justify-content: space-between;*/
     margin-bottom: 10px;
-    /*overflow-y: scroll;*/
-    overflow-y: hidden;
-    /*border: 5px solid #ff861c;*/
+    overflow-y:auto;
   }
-
-  .product_list_container {
-    position: relative;
-    bottom: 0;
-    overflow-x: scroll;
-    /*border: 2px solid #ff4418;*/
-  }
-
-  .mid_height {
-    position: relative;
-    bottom: 0;
-    overflow-x: scroll;
-    height: 48vh;
-    /*border: 2px solid #ecff1a;*/
-  }
-
-  .fullHeight {
-    position: relative;
-    bottom: 0;
-    overflow-x: scroll;
-    height: 78vh;
-    /*border: 2px solid #4325ff;*/
-  }
-
   .product-list {
     position: relative;
+    bottom: 0;
+    max-height: 100%;
     display: flex;
     flex-direction: row;
     flex-flow: wrap;
-    /*border: 5px solid #31fff8;*/
-    /*overflow-X: hidden;*/
   }
-
   .product-item {
     display: flex;
     flex-direction: column;
@@ -161,32 +130,19 @@ export default {
     cursor: pointer;
     z-index: 1;
     margin: 0 5px 5px 0;
-    /*border: 2px solid #4b0709;*/
   }
-
   .product-image {
     position: relative;
     height: 166px;
     width: 133px;
     background-color: grey;
-    /*border: 2px solid #297c1a;*/
   }
-
-  .loader-container {
-    position: relative;
-    width: 100vw;
-    display: flex;
-    justify-content: center;
-    /*border: 2px solid #ff03d3;*/
-  }
-
   .list-bottom-container{
     position: relative;
     display: flex;
     justify-content: center;
-    width: 100%
+    width: 100%;
   }
-
   .list-bottom {
     position: relative;
     height: 40px;
@@ -201,12 +157,6 @@ export default {
     outline: none;
     cursor: pointer;
   }
-
-  .list-bottom:hover{
-    background-color: #e1e1e1;
-    color : #777777;
-  }
-
   @media screen and (max-width: 970px)  {
     .list-bottom {
       width: 100%;
